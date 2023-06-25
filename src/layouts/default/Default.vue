412fa1fdf742b21ecdebd8ccb39b23027049aa34
renan martins <renanmartins989@gmail.com>
2023-06-25 03:45:21 +0100

    vuetify create with router + pinia + js + eslint

---
+++ src/layouts/default/Default.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-app-bar color="grey-darken-4">
+      <v-app-bar-title>Navbar</v-app-bar-title>
+    </v-app-bar>
+    <router-view />
+  </v-app>
+</template>
+
+<script setup>
+
+</script>
