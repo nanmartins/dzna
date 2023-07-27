32e3c471188878a67c97847ed14f301768f41f7a
renan martins <renanmartins989@gmail.com>
2023-07-27 23:32:04 +0100

    basic setup for i18n working

--- src/main.js
+++ src/main.js
@@ -1,12 +1,37 @@
 import { createApp } from 'vue'
 import { registerPlugins } from '@/plugins'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
+
 import App from './App.vue'
 import ThemeSwitch from '@/components/ThemeSwitch.vue'
 
+const messages = {
+  br: {
+    message: {
+      hello: 'Olá mundo'
+    }
+  },
+  en: {
+    message: {
+      hello: 'Hello world'
+    }
+  }
+}
+
+const i18n = createI18n({
+  locale: 'br',
+  fallbackLocale: 'en',
+  messages
+})
+
 const app = createApp(App)
 
+
 registerPlugins(app)
+
 app.component('ThemeSwitch', ThemeSwitch)
+
 app.use(createPinia)
+app.use(i18n)
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,28 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 import { defineConfig } from 'vite'
 import { fileURLToPath, URL } from 'node:url'
 
+// Vue i18n plugin
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({ 
+    vue({
       template: { transformAssetUrls }
     }),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
     vuetify({
       autoImport: true,
     }),
+    VueI18nPlugin({
+      include: [path.resolve(__dirname, './src/locales/**')],
+      // if you want to use Vue I18n Legacy API, you need to set `compositionOnly: false`
+      // compositionOnly: false,
+
+      // you need to set i18n resource including paths !
+      // include: resolve(dirname(fileURLToPath(import.meta.url)), './path/to/src/locales/**'),
+    })
   ],
   define: { 'process.env': {} },
   resolve: {
